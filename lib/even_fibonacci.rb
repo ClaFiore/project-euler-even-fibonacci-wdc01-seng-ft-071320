4c84276676c9adce876eb4da50285e4f9183e5f4
ClaFiore <claborghini@gmail.com>
2020-10-28 09:48:25 -0400

    fibonacci even methods

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,25 @@
-# Implement your procedural solution here!
+def fibonacci(num)
+    first = 0
+    second = 1
+    
+    (num - 1).times do
+        first, second = second, first + second
+        puts first
+    end
+    
+end
+
+# fibonacci(8) #returns eight number in the fibonacci sequence, with 0 as first
+
+def fibonacci_even(num)
+    first = 0
+    second = 1
+    fibonacci_sequence = []
+    (num - 1).times do
+        first, second = second, first + second
+        fibonacci_sequence.push(first)
+    end
+    even = fibonacci_sequence.filter {|n| n % 2 == 0}
+    puts even
+end
+fibonacci_even(8)
