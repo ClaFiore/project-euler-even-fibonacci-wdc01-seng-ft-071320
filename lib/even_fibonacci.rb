ff6396005338f8e1bdac0672a0d42bf7f88a7d1f
ClaFiore <claborghini@gmail.com>
2020-10-28 09:51:03 -0400

    fibonacci even with comments

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -3,13 +3,14 @@ def fibonacci(num)
     second = 1
     
     (num - 1).times do
-        first, second = second, first + second
+        first, second = second, first + second  #cannot split or it will give wrong result
         puts first
     end
     
 end
 
-# fibonacci(8) #returns eight number in the fibonacci sequence, with 0 as first
+fibonacci(8) #returns eight number in the fibonacci sequence, with 0 as first
+
 
 def fibonacci_even(num)
     first = 0
@@ -17,9 +18,10 @@ def fibonacci_even(num)
     fibonacci_sequence = []
     (num - 1).times do
         first, second = second, first + second
-        fibonacci_sequence.push(first)
+        fibonacci_sequence.push(first)  #sequence of first 8 Fibonacci numbers
     end
-    even = fibonacci_sequence.filter {|n| n % 2 == 0}
+    even = fibonacci_sequence.filter {|n| n % 2 == 0}  #filter only even numbers
     puts even
 end
+
 fibonacci_even(8)
